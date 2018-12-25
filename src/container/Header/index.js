683f80a5415a81f893c060c4e8788226fc27bb0c
YIPG <around.mr.child@gmail.com>
2018-12-25 14:26:41 +0900

    サイドバー追加

--- src/container/Header/index.js
+++ src/container/Header/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Menu from '@material-ui/core/Menu';
@@ -27,12 +34,27 @@ const styles = {
         marginLeft: -12,
         marginRight: 20,
     },
+    list: {
+        width: 200,
+      },
 };
+
+function ListItemLink(props) {
+    const { primary, to } = props;
+    return (
+      <li>
+        <ListItem button component={Link} to={to}>
+          <ListItemText primary={primary} />
+        </ListItem>
+      </li>
+    );
+  }
 
 class ButtonAppBar extends React.Component {
     
     state = {
         anchorEl: null,
+        drawer: false,
     };
 
     handleMenu = event => {
@@ -58,27 +80,60 @@ class ButtonAppBar extends React.Component {
         history.push('/')
     }
 
+    handleToHome = () => {
+        history.push('/')
+    }
+
+    toggleDrawer = open => () => {
+        this.setState({
+          drawer: open,
+        });
+      };
+
     render() {
         const { classes, user } = this.props;
         const { anchorEl } = this.state;
         const open = Boolean(anchorEl);
 
-        const handleToHome = () => {
-            history.push('/')
-        }
+        const sideList = (
+            <div className={classes.list}>
+                <List>
+                    <ListItemLink to='/login' primary='ログイン' />
+                </List>
+                <Divider />
+                <List>
+                    <ListItemLink to='/welcome' primary='ウェルカム' />
+                    <ListItemLink to='/map' primary='地図' />
+                </List>
+            </div>
+        );
 
         return (
             <div className={classes.root}>
                 <AppBar position="static">
                     <Toolbar>
-                        <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                        <IconButton className={classes.menuButton} onClick={this.toggleDrawer(true)} color="inherit" aria-label="Menu">
                             <MenuIcon />
                         </IconButton>
+                        <SwipeableDrawer
+                            open={this.state.drawer}
+                            onClose={this.toggleDrawer(false)}
+                            onOpen={this.toggleDrawer(true)}
+                        >
+                            <div
+                                tabIndex={0}
+                                role="button"
+                                onClick={this.toggleDrawer(false)}
+                                onKeyDown={this.toggleDrawer(false)}
+                            >
+                                {sideList}
+                            </div>
+                        </SwipeableDrawer>
                         <Typography
                             variant="h6"
                             color="inherit"
                             className={classes.grow}
-                            onClick={handleToHome}
+                            onClick={this.handleToHome}
                         >
                             Ubivis
                         </Typography>
