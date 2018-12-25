cecfb76d6401a845043177041fc4dc47b60681f9
YIPG <around.mr.child@gmail.com>
2018-12-25 14:30:29 +0900

    不要なモジュール除去

--- src/container/Header/index.js
+++ src/container/Header/index.js
@@ -12,7 +12,6 @@ import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import Divider from '@material-ui/core/Divider';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
