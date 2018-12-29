df07defb1f0f6798786bdcd79d22e890de030bd7
YIPG <around.mr.child@gmail.com>
2018-12-29 16:26:19 +0900

    auth()のつけ忘れ

--- src/Route/PrivateRoute.js
+++ src/Route/PrivateRoute.js
@@ -3,13 +3,14 @@ import {  Route, Redirect } from 'react-router-dom';
 import firebase from 'firebase';
 
 function PrivateRoute({ component: Component, ...rest }) {
+  console.log(firebase.auth().currentUser);
   return (
     <Route
       {...rest}
       render={props => 
         // ここで認証状態を取得します。
         // firebaseだったらfirebase.auth.currentUser !== nullとかで同様になります。
-        firebase.auth.currentUser !== null ? (
+        firebase.auth().currentUser !== null ? (
           // ログイン済みならば、PrivateRouteに渡されたcomponentを返します。
           <Component {...props} />
         ) : (
